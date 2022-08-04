3ed9d20c2ad86916a5e4c9ecf3f06f96a43e03d3
Kausik Chattapadhyay <chat.kausik@gmail.com>
2022-08-04 10:32:30 -0400

    commit

--- chattapadhyay_9.1.py
+++ chattapadhyay_9.1.py
@@ -1,14 +1,37 @@
 # Author: Kausik Chattapadhyay
 # DSC 510 Assignment 9.1
 # Date 08/03/2022
-# This program is to access Chuck Norris jokes from the web and
+# This program is to access Chuck Norris jokes from the web API and
 # display them for the user of the program.
 
 import requests
 from pprint import pprint
 
-if __name__ == '__main__':
 
+def fetch_joke(category):
+    """
+    Get the jokes from the API, jsonify the data and extract the joke text from the json values.
+    :param category:
+    :return:
+    """
+    try:
+        req_joke = requests.get(f"https://api.chucknorris.io/jokes/random?={category}")
+        req_joke.raise_for_status()
+        data = req_joke.json()
+        print("\n" + data["value"])
+    except requests.exceptions.HTTPError as error:
+        print(f'HTTP error occurred: {error}')
+    except requests.ConnectionError as error:
+        print(f'Connection error occurred: {error}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+
+def main():
+    """
+    User will enter the category and the corresponding jokes will be fetched from the API.
+    :return:
+    """
     print("""Welcome. Please choose your Chuck Norris joke of the day!  
     (0) End Program      (1) Animal      (2) Career 
     (3) Celebrity        (4) Dev         (5) Explicit  
@@ -25,7 +48,7 @@ if __name__ == '__main__':
     while True:
         choose = None
         try:
-            choose = int(input("What category would you like? "))
+            choose = int(input("What category would you like? Enter 0 to exit. "))
         except ValueError as e:
             pass
 
@@ -36,21 +59,14 @@ if __name__ == '__main__':
                 print(f'Category must be integer: {ex}')
 
             category = cat_dict[choose]
-
-            try:
-                req_joke = requests.get(f"https://api.chucknorris.io/jokes/random?={category}")
-                req_joke.raise_for_status()
-                data = req_joke.json()
-                print("\n" + data["value"])
-            except requests.exceptions.HTTPError as error:
-                print(f'HTTP error occurred: {error}')
-            except requests.ConnectionError as error:
-                print(f'Connection error occurred: {error}')
-            except Exception as err:
-                print(f'Other error occurred: {err}')
+            fetch_joke(category)   # Fetch the jokes from the API, jsonify and extract the joke text.
 
         elif choose == 0:
             print("See you later!")
             break
         else:
             print("Invalid input. Please try again.")
+
+
+if __name__ == '__main__':
+    main()
